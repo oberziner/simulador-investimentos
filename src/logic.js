dfc9d95bad26d1c49aa8af5f9750ea331186a545
Augusto Oberziner <augustoberziner@gmail.com>
2019-12-08 15:52:41 -0300

    Removing unused files

--- src/logic.js
+++
@@ -1,4 +0,0 @@
-export default {
-
-  getHelloMessage: () => 'Hello',
-};
--- src/logic.test.js
+++
@@ -1,8 +0,0 @@
-import logic from './logic';
-
-describe('logic should', () => {
-  it('return Hello', () => {
-    expect.assertions(1);
-    expect(logic.getHelloMessage()).toBe('Hello');
-  });
-});
