a1d575d1f3413603466cd05959dfb58a4a1cad94
Augusto Oberziner <augustoberziner@gmail.com>
2019-12-10 17:12:53 -0300

    LCI object

--- src/simulation/lci.js
+++ src/simulation/lci.js
@@ -1,10 +1,27 @@
 import f from './sequence-factory';
 import dates from './dates';
 
-export const newLCI = (initialDate, initialValue, rate) => f.newSequence(
+export const newLCISeq = (initialDate, initialValue, rate) => f.newSequence(
   { date: initialDate, value: initialValue },
   (prev) => ({
     date: dates.getNextDay(prev.date),
     value: prev.value * (1 + rate.dailyRate()),
   }),
 );
+
+export const newLCI = (startDate, initialValue, rate, endDate) => {
+  const seq = newLCISeq(startDate, initialValue, rate);
+  const steps = [];
+
+  for (let i = seq.next(); ((i.date < endDate)); i = seq.next()) {
+    steps.push(i);
+  }
+
+  return {
+    title: 'LCI',
+    startDate,
+    endDate,
+    initialValue,
+    steps,
+  };
+};
--- src/simulation/lci.test.js
+++ src/simulation/lci.test.js
@@ -1,16 +1,16 @@
-import { newLCI } from './lci';
+import { newLCISeq, newLCI } from './lci';
 import { newRate } from './interest-rates';
 
 describe('lci sequence', () => {
   it('first .next should return the invested value', () => {
-    expect(newLCI(new Date('2019-06-01'), 1000).next()).toStrictEqual({
+    expect(newLCISeq(new Date('2019-06-01'), 1000).next()).toStrictEqual({
       date: new Date('2019-06-01'),
       value: 1000,
     });
   });
 
   it('subsequent .next should return the updated value for each day', () => {
-    const lci = newLCI(new Date('2019-07-01'), 1000, newRate(0.05, 'year'));
+    const lci = newLCISeq(new Date('2019-07-01'), 1000, newRate(0.05, 'year'));
     lci.next(); // jump first
     let { date, value } = lci.next();
     expect(date).toStrictEqual(new Date('2019-07-02'));
@@ -27,5 +27,31 @@ describe('lci sequence', () => {
     ({ date, value } = lci.next());
     expect(date).toStrictEqual(new Date('2019-07-05'));
     expect(value).toBeCloseTo(1000.77, 2);
+  });
+});
+
+describe('lci object', () => {
+  const lci = newLCI(new Date('2019-07-01'), 1000, newRate(0.05, 'year'), new Date('2019-10-01'));
+
+  it('should have a title', () => {
+    expect(lci.title).toBe('LCI');
+  });
+  it('should have a startDate', () => {
+    expect(lci.startDate).toStrictEqual(new Date('2019-07-01'));
+  });
+  it('should have an endDate', () => {
+    expect(lci.endDate).toStrictEqual(new Date('2019-10-01'));
+  });
+  it('should have an initialValue', () => {
+    expect(lci.initialValue).toBe(1000);
+  });
+  it('should have a list of steps', () => {
+    expect(lci.steps).toHaveLength(92);
+  });
+  it('should have a initial date as first step', () => {
+    expect(lci.steps[0].date).toStrictEqual(new Date('2019-07-01'));
+  });
+  it('should have the day before the end date as last step', () => {
+    expect(lci.steps[lci.steps.length - 1].date).toStrictEqual(new Date('2019-09-30'));
   });
 });
