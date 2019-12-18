c9e5a1adb2210ecb44ce206ffb17d322db7582bd
Augusto Oberziner <augustoberziner@gmail.com>
2019-12-18 16:43:55 -0300

    Created buttons to add CBD and Tesouro

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,67 @@ import Investment from './Investment';
 import './main.css';
 import { newRate } from './simulation/interest-rates';
 import { newLCI } from './simulation/lci';
+import { newCDB } from './simulation/cdb';
+import { newTesouro } from './simulation/tesouro';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.addLCI = this.addInvestment.bind(this, this.newLCI);
+    this.addLCI = this.addInvestment.bind(this);
+    this.addCDB = this.addInvestmentCDB.bind(this);
+    this.addTesouro = this.addInvestmentTesouro.bind(this);
 
     this.state = {
+      investments: [],
     };
   }
 
   addInvestment() {
-    this.setState(() => ({
-      investment: newLCI(
+    this.setState((state) => ({
+      investments: [...state.investments, newLCI(
         new Date('2019-04-01'),
-        10000,
+        9999,
         newRate(0.04, 'year252'),
         new Date('2019-12-01'),
-      ),
+      )],
+    }));
+  }
+
+  addInvestmentCDB() {
+    this.setState((state) => ({
+      investments: [...state.investments, newCDB(
+        new Date('2019-04-01'),
+        8888,
+        newRate(0.04, 'year252'),
+        new Date('2019-12-01'),
+      )],
+    }));
+  }
+
+  addInvestmentTesouro() {
+    this.setState((state) => ({
+      investments: [...state.investments, newTesouro(
+        new Date('2019-04-01'),
+        5000,
+        newRate(0.05, 'year252'),
+        new Date('2019-12-01'),
+      )],
     }));
   }
 
   render() {
-    const { investment } = this.state;
+    const { investments } = this.state;
     return (
       <div>
         <button type="button" onClick={this.addLCI}>LCI</button>
-        { investment !== undefined
-        && <Investment investment={investment} /> }
+        <button type="button" onClick={this.addCDB}>CDB</button>
+        <button type="button" onClick={this.addTesouro}>Tesouro</button>
+        {investments.map((i) => (
+          <div key={Math.random()}>
+            <Investment investment={i} />
+          </div>
+        ))}
       </div>
     );
   }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import App from './App';
 import '@testing-library/jest-dom/extend-expect';
 
-
 describe('app', () => {
   it('should have a button to add a new LCI investment', () => new Promise((done) => {
     const { getByRole, getByText } = render(<App />);
@@ -14,10 +13,69 @@ describe('app', () => {
 
     waitForElement(() => getByRole('heading'))
       .then((element) => {
-        expect(element.parentNode).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$|u00A09.999,00$/);
-        expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 10.268,122/,
+        expect(element.parentNode).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$ 9.999,00/,
           { normalizeWhitespace: true });
-        expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 10.000,00$/,
+        expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 10.267,092/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 9.999,00$/,
+          { normalizeWhitespace: true });
+        done();
+      });
+  }));
+
+  it('should add 2 LCIs when LCI button is clicked 2 times', () => new Promise((done) => {
+    const { getAllByRole, getByText } = render(<App />);
+    const lciButton = getByText((text, element) => (text === 'LCI')
+      && (element.tagName === 'BUTTON'));
+
+    lciButton.click();
+    lciButton.click();
+
+    waitForElement(() => getAllByRole('heading'))
+      .then((elements) => {
+        expect(elements).toHaveLength(2);
+
+        expect(elements[0].parentNode).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$ 9.999,00/,
+          { normalizeWhitespace: true });
+        expect(elements[1].parentNode).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$ 9.999,00/,
+          { normalizeWhitespace: true });
+        done();
+      });
+  }));
+
+  it('should have a button to add a new CDB investment', () => new Promise((done) => {
+    const { getByRole, getByText } = render(<App />);
+    const cdbButton = getByText((text, element) => (text === 'CDB')
+      && (element.tagName === 'BUTTON'));
+
+    cdbButton.click();
+
+    waitForElement(() => getByRole('heading'))
+      .then((element) => {
+        expect(element.parentNode).toHaveTextContent(/^CDBData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$ 8.888,00/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 9.126,302/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 8.888,00$/,
+          { normalizeWhitespace: true });
+        done();
+      });
+  }));
+
+  it('should have a button to add a new Tesouro investment', () => new Promise((done) => {
+    const { getByRole, getByText } = render(<App />);
+    const tesouroButton = getByText((text, element) => (text === 'Tesouro')
+      && (element.tagName === 'BUTTON'));
+
+    tesouroButton.click();
+
+    waitForElement(() => getByRole('heading'))
+      .then((element) => {
+        expect(element.parentNode).toHaveTextContent(/^Tesouro DiretoData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$ 5.000,00/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 5.167,312/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 5.000,00$/,
           { normalizeWhitespace: true });
         done();
       });
