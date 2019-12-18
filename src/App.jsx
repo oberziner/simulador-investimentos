ed938095d86d9b5c59668270857dd777c7e2e07c
Augusto Oberziner <augustoberziner@gmail.com>
2019-12-17 21:21:02 -0300

    Created button to add LCI

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.addLCI = this.addInvestment.bind(this, this.newLCI);
+
     this.state = {
+    };
+  }
+
+  addInvestment() {
+    this.setState(() => ({
       investment: newLCI(
         new Date('2019-04-01'),
         10000,
         newRate(0.04, 'year252'),
         new Date('2019-12-01'),
       ),
-    };
+    }));
   }
 
   render() {
     const { investment } = this.state;
     return (
-      <Investment investment={investment} />
+      <div>
+        <button type="button" onClick={this.addLCI}>LCI</button>
+        { investment !== undefined
+        && <Investment investment={investment} /> }
+      </div>
     );
   }
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 import '@testing-library/jest-dom/extend-expect';
 
 
 describe('app', () => {
-  it('should render investments correctly', () => {
-    expect.hasAssertions();
-    const { getByRole, container } = render(<App />);
+  it('should have a button to add a new LCI investment', () => new Promise((done) => {
+    const { getByRole, getByText } = render(<App />);
+    const lciButton = getByText((text, element) => (text === 'LCI')
+      && (element.tagName === 'BUTTON'));
 
-    expect(container).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$|u00A09.999,00$/);
+    lciButton.click();
 
-    expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 10.268,122/,
-      { normalizeWhitespace: true });
-    expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 10.000,00$/,
-      { normalizeWhitespace: true });
-  });
+    waitForElement(() => getByRole('heading'))
+      .then((element) => {
+        expect(element.parentNode).toHaveTextContent(/^LCIData inicio: 2019-04-01Data fim: 2019-12-01Valor inicial: R\$|u00A09.999,00$/);
+        expect(getByRole('table')).toHaveTextContent(/^1 - 2019-11-30R\$ 10.268,122/,
+          { normalizeWhitespace: true });
+        expect(getByRole('table')).toHaveTextContent(/244 - 2019-04-01R\$ 10.000,00$/,
+          { normalizeWhitespace: true });
+        done();
+      });
+  }));
 });
