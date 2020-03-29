38c2f23eae3f02bf2eee7dd689c4b93a3c272c37
Augusto Oberziner <augustoberziner@gmail.com>
2020-03-29 16:23:53 -0300

    Adding selic list

---
+++ files/selic/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const fileStream = fs.createReadStream('selic_diaria.csv');
+
+const rl = readline.createInterface({
+  input: fileStream,
+  crlfDelay: Infinity
+});
+
+const parseDate = (dateStr) => {
+  const [day, month, year] = dateStr.split("/");
+  return `${year}-${month}-${day}`
+}
+
+rl.on('line', (input) => {
+  const values = input.split(";");
+  const date = parseDate(values[0]);
+  const yearlySelic = values[1].replace(',', '.');
+  const dailySelic = values[2].replace(',', '.');
+  const object = {
+    date,
+    yearlySelic,
+    dailySelic
+  }
+  console.log(JSON.stringify(object) + ",");
+});
+
+rl.on('close', () => {
+  console.log("acabo");
+});
