c4f329b132da4a9459e2a21f6524853efc271844
Augusto Oberziner <augustoberziner@gmail.com>
2020-04-26 16:10:16 -0300

    More (possibly not better) style

--- src/Investment.jsx
+++ src/Investment.jsx
@@ -6,27 +6,34 @@ const Investment = ({ investment, onRemoveClick }) => (
     <h2 className="investment-title">
       {investment.title}
     </h2>
-    <p>
-      {`Data inicio: ${f.formatDate(investment.startDate)}`}
-    </p>
-    <p>
-      {`Data fim: ${f.formatDate(investment.endDate)}`}
-    </p>
-    <p>
-      {`Valor inicial: ${f.formatMoney(investment.initialValue)}`}
-    </p>
-    <p>
-      {`Valor Bruto: ${f.formatMoney(investment.grossValue)}`}
-    </p>
-    <p>
-      {`Impostos: ${f.formatMoney(investment.totalTaxes)}`}
-    </p>
-    <p>
-      {`Taxa de Custodia: ${f.formatMoney(investment.totalCustodyFee)}`}
-    </p>
-    <p>
-      {`Valor Liquido: ${f.formatMoney(investment.netValue)}`}
-    </p>
+    <div className="investment-line">
+      <p> {`Data Inicio:`} </p>
+      <p> {`${f.formatDate(investment.startDate)}`} </p>
+    </div>
+    <div className="investment-line">
+      <p> {`Data fim:`} </p>
+      <p> {`${f.formatDate(investment.endDate)}`} </p>
+    </div>
+    <div className="investment-line">
+      <p> {`Valor inicial:`} </p>
+      <p> {`${f.formatMoney(investment.initialValue)}`} </p>
+    </div>
+    <div className="investment-line">
+      <p> {`Valor Bruto:`} </p>
+      <p> {`${f.formatMoney(investment.grossValue)}`} </p>
+    </div>
+    <div className="investment-line">
+      <p> {`Impostos:`} </p>
+      <p> {`${f.formatMoney(investment.totalTaxes)}`} </p>
+    </div>
+    <div className="investment-line">
+      <p> {`Taxa de Custodia:`} </p>
+      <p> {`${f.formatMoney(investment.totalCustodyFee)}`} </p>
+    </div>
+    <div className="investment-line net-value">
+      <p> {`Valor Liquido:`} </p>
+      <p> {`${f.formatMoney(investment.netValue)}`} </p>
+    </div>
     <button className="remove-button" type="button" onClick={onRemoveClick.bind(null, investment)}>Remover</button>
     <table>
       <tbody>
