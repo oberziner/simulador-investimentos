0bed24a2edf1c87aa6ff06e6108b345e482145f1
Augusto Oberziner <augustoberziner@gmail.com>
2020-04-26 16:19:53 -0300

    Lint

--- src/Investment.jsx
+++ src/Investment.jsx
@@ -7,32 +7,32 @@ const Investment = ({ investment, onRemoveClick }) => (
       {investment.title}
     </h2>
     <div className="investment-line">
-      <p> {`Data Inicio:`} </p>
-      <p> {`${f.formatDate(investment.startDate)}`} </p>
+      <p>Data inicio: </p>
+      <p>{`${f.formatDate(investment.startDate)}`}</p>
     </div>
     <div className="investment-line">
-      <p> {`Data fim:`} </p>
-      <p> {`${f.formatDate(investment.endDate)}`} </p>
+      <p>Data fim: </p>
+      <p>{`${f.formatDate(investment.endDate)}`}</p>
     </div>
     <div className="investment-line">
-      <p> {`Valor inicial:`} </p>
-      <p> {`${f.formatMoney(investment.initialValue)}`} </p>
+      <p>Valor inicial: </p>
+      <p>{`${f.formatMoney(investment.initialValue)}`}</p>
     </div>
     <div className="investment-line">
-      <p> {`Valor Bruto:`} </p>
-      <p> {`${f.formatMoney(investment.grossValue)}`} </p>
+      <p>Valor Bruto: </p>
+      <p>{`${f.formatMoney(investment.grossValue)}`}</p>
     </div>
     <div className="investment-line">
-      <p> {`Impostos:`} </p>
-      <p> {`${f.formatMoney(investment.totalTaxes)}`} </p>
+      <p>Impostos: </p>
+      <p>{`${f.formatMoney(investment.totalTaxes)}`}</p>
     </div>
     <div className="investment-line">
-      <p> {`Taxa de Custodia:`} </p>
-      <p> {`${f.formatMoney(investment.totalCustodyFee)}`} </p>
+      <p>Taxa de Custodia: </p>
+      <p>{`${f.formatMoney(investment.totalCustodyFee)}`}</p>
     </div>
     <div className="investment-line net-value">
-      <p> {`Valor Liquido:`} </p>
-      <p> {`${f.formatMoney(investment.netValue)}`} </p>
+      <p>Valor Liquido: </p>
+      <p>{`${f.formatMoney(investment.netValue)}`}</p>
     </div>
     <button className="remove-button" type="button" onClick={onRemoveClick.bind(null, investment)}>Remover</button>
     <table>
